0666186e63e521f411727b02975f7839d2d89f31
Shailendr Shrivastav <55743907+mysterious-shailendr@users.noreply.github.com>
2022-01-16 18:36:04 +0530

    Update login.py

--- src/login.py
+++ src/login.py
@@ -16,7 +16,7 @@ def _login_auth(url: str):
     br.addheaders = [('User-agent', 'Chrome')]
 
     # The site we will navigate into, handling it's session
-    br.open('https://account.mekari.com/users/sign_in?client_id=JUR-7645867921&return_to=L2F1dGg_Y2xpZW50X2lkPUpVUi03NjQ1ODY3OTIxJnJlc3BvbnNlX3R5cGU9Y29kZSZzY29wZT1zc286cHJvZmlsZQ%3D%3D')
+    br.open('https://account.mekari.com/users/sign_in')
 
     # View available forms
     login_form = br.select_form(id="new_user")
